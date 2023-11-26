223aeee508171f3d1478fca836adfc932ad371e7
yukimura-manase <masahiro19951212@gmail.com>
2023-11-26 13:04:25 +0900

    ExcelからCSVに変換する処理

---
+++ excel-to-csv.py
@@ -0,0 +1,21 @@
+# Excel を CSV に変換する Module
+
+import pandas as pd
+import os
+
+excel_file_path = ''
+create_csv_file_path = ''
+
+excel_file = '業種分類.xlsx'
+csv_file = '業種分類.csv'
+
+# Excel は、カレントに Setしてある前提
+excel_file_path = f'{os.getcwd()}/{excel_file}'
+create_csv_file_path = f'{os.getcwd()}/{csv_file}'
+
+print(excel_file_path)
+
+print(create_csv_file_path)
+
+read_file = pd.read_excel(excel_file_path)
+read_file.to_csv(create_csv_file_path, index = None, header=True)
